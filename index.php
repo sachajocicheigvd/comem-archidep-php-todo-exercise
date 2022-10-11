59b54b6bfb102fb11669109a26b8bcc4f75e3fb1
lchollet <leo.chollet.2000@gmail.com>
2022-10-11 12:35:03 +0200

    change of db port line 15

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ define('DB_USER', 'todolist');
 define('DB_PASS', 'chAngeMeN0w!');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3306');
+define('DB_PORT', '8888');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
