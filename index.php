d87e8605df14af53dcae277dc7c860b7087796af
Sacha Jocic <jocic.sacha@gmail.com>
2022-10-11 11:00:20 +0200

    correction de la ligne 80 pour le select correct dans la db

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME
+$selectQuery = 'SELECT from todo'; // IMPLEMENT ME
 $items = $db->query($selectQuery);
 ?>
 
