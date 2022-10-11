cb95290bbd1125949dee490a11f80df143f4f7fa
lchollet <leo.chollet.2000@gmail.com>
2022-10-11 12:49:19 +0200

    re-edit of the base URL line 8

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
 // "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
 // should be "/comem-archidep-php-todo-exercise/". If you are accessing the
 // application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/comem-archidep-php-todo-exercise/');
+define('BASE_URL', '/');
 
 // Database connection parameters.
 define('DB_USER', 'todolist');
 define('DB_PASS', 'chAngeMeN0w!');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '8888');
+define('DB_PORT', '8889');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = 'SELECT from todo'; // IMPLEMENT ME
+$selectQuery = 'SELECT * from todo'; // IMPLEMENT ME
 $items = $db->query($selectQuery);
 ?>
 
